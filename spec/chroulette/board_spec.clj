10ad57db603736f8d0457a14b54c474e024eaa60
Sasha & Tatiana <koss+shepik@nocorp.me>
2014-04-04 22:25:41 +0400

    new-board function is implemented

--- spec/chroulette/board_spec.clj
+++ spec/chroulette/board_spec.clj
@@ -3,4 +3,99 @@
     [speclj.core :refer :all]
     [chroulette.board :refer :all]))
 
+(describe "valid-position?"
+  (it "returns true if position is vector and contain two numbers"
+    (should (valid-position? [1 5]))
+    (should-not (valid-position? [1]))
+    (should-not (valid-position? [1 2 3]))
+    (should-not (valid-position? [true \a]))))
+
+(describe "black-square?"
+  (it "returns true it square for given position is black"
+    (should (black-square? [2 1]))
+    (should-not (black-square? [7 7]))))
+
+(describe "on-board?"
+  (it "returns true if position is on board"
+    (should (on-board? [4 6]))
+    (should-not (on-board? [0 1]))
+    (should-not (on-board? [1 9]))))
+
+(describe "new-piece"
+  (it "returns map with given position and :king false"
+    (should= {:position [4 2] :king false} (new-piece [4 2]))))
+
+(describe "next-square"
+  (it "returns position of next square"
+    (should= [2 1] (next-square [1 1]))
+    (should= [1 2] (next-square [8 1]))))
+
+(describe "start-row-for"
+  (it "returns start row for given colour"
+    (should= 1 (start-row-for :white))
+    (should= 6 (start-row-for :black))))
+
+(describe "initial-pieces"
+  (it "returns vector of inital state for given colour"
+    (should=
+      [{:position [2 1] :king false}
+       {:position [4 1] :king false}
+       {:position [6 1] :king false}
+       {:position [8 1] :king false}
+       {:position [1 2] :king false}
+       {:position [3 2] :king false}
+       {:position [5 2] :king false}
+       {:position [7 2] :king false}
+       {:position [2 3] :king false}
+       {:position [4 3] :king false}
+       {:position [6 3] :king false}
+       {:position [8 3] :king false}]
+      (initial-pieces :white))
+    (should=
+      [{:position [1 6] :king false}
+       {:position [3 6] :king false}
+       {:position [5 6] :king false}
+       {:position [7 6] :king false}
+       {:position [2 7] :king false}
+       {:position [4 7] :king false}
+       {:position [6 7] :king false}
+       {:position [8 7] :king false}
+       {:position [1 8] :king false}
+       {:position [3 8] :king false}
+       {:position [5 8] :king false}
+       {:position [7 8] :king false}]
+      (initial-pieces :black))
+    ))
+
+(describe "new-board"
+  (it "returns inital state of board"
+    (should=
+      {:white
+       [{:position [2 1] :king false}
+        {:position [4 1] :king false}
+        {:position [6 1] :king false}
+        {:position [8 1] :king false}
+        {:position [1 2] :king false}
+        {:position [3 2] :king false}
+        {:position [5 2] :king false}
+        {:position [7 2] :king false}
+        {:position [2 3] :king false}
+        {:position [4 3] :king false}
+        {:position [6 3] :king false}
+        {:position [8 3] :king false}]
+       :black
+       [{:position [1 6] :king false}
+        {:position [3 6] :king false}
+        {:position [5 6] :king false}
+        {:position [7 6] :king false}
+        {:position [2 7] :king false}
+        {:position [4 7] :king false}
+        {:position [6 7] :king false}
+        {:position [8 7] :king false}
+        {:position [1 8] :king false}
+        {:position [3 8] :king false}
+        {:position [5 8] :king false}
+        {:position [7 8] :king false}]}
+      (new-board))))
+
 (run-specs)
--- src/chroulette/board.clj
+++ src/chroulette/board.clj
@@ -1,1 +1,58 @@
 (ns chroulette.board)
+
+(defn valid-position?
+  [position]
+  (and
+    (= (count position) 2)
+    (number? (position 0))
+    (number? (position 1))))
+
+(defn black-square?
+  [position]
+  (let
+    [column (position 0)
+     row (position 1)]
+    (or
+      (and (odd? column) (even? row))
+      (and (even? column) (odd? row)))))
+
+(defn on-board?
+  [position]
+  (let
+    [column (position 0)
+     row (position 1)]
+    (and
+      (and (> column 0) (< column 9))
+      (and (> row 0) (< row 9)))))
+
+(defn new-piece [position] {:position position :king false})
+
+(defn start-row-for [colour] ({:white 1 :black 6} colour))
+
+(defn next-square
+  [position]
+  (let
+    [column (position 0)
+     row (position 1)
+     square-on-rigth [(inc column) row]
+     ]
+    (if (on-board? square-on-rigth)
+      square-on-rigth
+      [1 (inc row)])))
+
+(defn initial-pieces
+  [colour]
+  (loop
+    [position [1 (start-row-for colour)]
+     pieces []]
+    (if (= 12 (count pieces))
+      pieces
+      (recur
+        (next-square position)
+        (if (black-square? position)
+          (conj pieces (new-piece position))
+          pieces)))))
+
+(defn new-board []
+  {:white (initial-pieces :white)
+   :black (initial-pieces :black)})
