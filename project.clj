2cdafd6f44d7d3fd05ae45ce0987197dfb0812c4
Sasha & Tatiana <koss+shepik@nocorp.me>
2014-04-05 11:36:19 +0400

    Ring's hello world

--- project.clj
+++ project.clj
@@ -1,5 +1,7 @@
 (defproject chroulette "0.1.0"
-  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [ring/ring-core "1.2.2"]
+                 [ring/ring-jetty-adapter "1.2.2"]]
   :profiles {:dev {:dependencies [[speclj "3.0.2"]]}}
   :plugins [[speclj "3.0.2"]]
   :test-paths ["spec"])
